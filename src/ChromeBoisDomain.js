739c642d69db0b26c3a09461069c08009f11fa9e
Harmandeep S. Mand <harmandeepmand.hm@gmail.com>
2020-05-24 12:27:32 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -34,7 +34,7 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
 
-  handleKeyPresses = (event) => {
+  handleKeyPresses =  (event) => {
     if(event.key === 'a'){
       resize("+")
     }
